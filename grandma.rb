205e9e0e117b66f02e67ca5ac9baa85be17c6347
Ashleigh Chun <ashleigh.chun@wework.com>
2018-09-23 02:41:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  if PUMPKIN!"
   elsif phrase.capitalize
     return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
-
+phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO 
   end
 end
     
