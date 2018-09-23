062e09efd2ca2797d78c6627c660c6302bfe3d64
Ashleigh Chun <ashleigh.chun@wework.com>
2018-09-23 02:58:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase != phrase.upcase
+  else phrase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
